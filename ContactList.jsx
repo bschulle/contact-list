1248e9f28202b0bf039b6789f7c5bdc35a6e07e2
Bryce <bryceschulle@yahoo.com>
2025-01-14 16:20:23 -0800

    initial commit

---
+++ ContactList.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+const ContactList = () => {
+  useEffect(() => {
+    
+  }, []);
+  
+  return (
+    <>
+      <h1>Contact List</h1>
+    </>
+  )
+}
+
+export default ContactList
+git remote add origin https://github.com/bschulle/contact-list.git
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ContactList from "../ContactList"
+
+const App = () => {
+
+  return (
+    <>
+      <h1>My Favorite Contacts</h1>
+      <ContactList />
+    </>
+  )
+}
+
+export default App
